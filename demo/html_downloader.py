a41cb15f82fbb24137b5c1643483d3e3779629b1
塞尔达一生推 <32028359+ghq120@users.noreply.github.com>
2018-06-09 17:22:16 +0800

    Add files via upload

---
+++ demo/html_downloader.py
@@ -0,0 +1,18 @@
+import urllib
+
+class HtmlDownloader(object):
+
+    def download(self, url):
+        if url is None:
+            print('url is None')
+            return None
+        try:
+            response = urllib.request.urlopen(url, timeout=10)
+            if response.getcode() != 200:
+                print('false')
+                return None
+            print('success')
+        except:
+            print('timeout')
+
+        return response.read()
